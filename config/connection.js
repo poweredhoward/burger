e9794d0cb1e6125d6d072af06a235113c429cdfe
mhoward <matthew.howard96@yahoo.com>
2018-06-26 17:06:16 -0700

    Enable Jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    database: "tasks_db",
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "admin"
-});
+
+var connection;
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        database: "tasks_db",
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "admin"
+    });
+}
+
 
 connection.connect(function (err){
     if(err) console.log(err.stack);
